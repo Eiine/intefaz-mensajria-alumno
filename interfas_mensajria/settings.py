5e55948efad38a0faaff895ad41e3cd963efbab9
Miguel <yannokaiserfrom@gmail.com>
2025-11-05 01:46:28 -0300

    add login

--- interfas_mensajria/settings.py
+++ interfas_mensajria/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,8 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Esto crea una carpeta 'media' en el directorio raíz del proyecto.
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# URL pública que se usará para referenciar los archivos media
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
